92d245f0fcac3cc77e2ed0fb40846de3fb59b96e
Clay Jensen-Reimann <clayreimann@gmail.com>
2019-03-30 12:15:27 -0500

    Initial commit
    - Thrift@11 hopefully installed as thrift-11

---
+++ Formula/thrift-11.rb
@@ -0,0 +1,50 @@
+class Thrift11 < Formula
+  desc "Framework for scalable cross-language services development"
+  homepage "https://thrift.apache.org/"
+  url "https://archive.apache.org/dist/thrift/0.11.0/thrift-0.11.0.tar.gz"
+  sha256 "c4ad38b6cb4a3498310d405a91fef37b9a8e79a50cd0968148ee2524d2fa60c2"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "bison" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "openssl"
+
+  def install
+    system "./bootstrap.sh" unless build.stable?
+
+    args = %W[
+      --disable-debug
+      --disable-tests
+      --prefix=#{prefix}
+      --libdir=#{lib}
+      --with-openssl=#{Formula["openssl"].opt_prefix}
+      --without-erlang
+      --without-haskell
+      --without-java
+      --without-perl
+      --without-php
+      --without-php_extension
+      --without-python
+      --without-ruby
+    ]
+
+    ENV.cxx11 if ENV.compiler == :clang
+
+    # Don't install extensions to /usr:
+    ENV["PY_PREFIX"] = prefix
+    ENV["PHP_PREFIX"] = prefix
+    ENV["JAVA_PREFIX"] = buildpath
+
+    system "./configure", *args
+    ENV.deparallelize
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/thrift-11", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# Clay's Homebrew Taps
+
+Taps for the Homebrew package manager.
+
+Taps for the Linuxbrew package manager (Homebrew on Linux or Windows 10 Subsystem for Linux) are in [Linuxbrew/homebrew-core](https://github.com/Linuxbrew/homebrew-core).
+
+## How do I install these formulae?
+
+Just `brew install clayreimann/brew-taps/<formula>`. This is an alternate tap for Homebrew.
+
+## More Documentation, Troubleshooting, Contributing, Security, Community, Donations, License and Sponsors
+
+See these sections in [Homebrew/brew's README](https://github.com/Homebrew/brew#homebrew).
